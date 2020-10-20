8809c86732a337af39afa81ee90dccad0e30e8d8
Ksenia Tsubulko <tsubulko.ksenia@gmail.com>
2020-10-21 00:50:03 +0300

    Added new tests

---
+++ Faker.UsageExample/NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Faker.UsageExample
+{
+    public class NewClass
+    {
+        public DateTime dateTimeField;
+        public char stringField;
+        public object objectField;
+        public SecondClass againclass;
+    }
+}
---
+++ Faker.UsageExample/SecondClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Faker.UsageExample
+{
+    public class SecondClass
+    {
+        public DateTime DateTimeProperty
+            { get; set; }
+            public string StringProperty
+            { get; set; }
+            public object ObjectProperty
+            { get; set; }
+
+            public ExampleClassProperties newclass;
+    }
+}
